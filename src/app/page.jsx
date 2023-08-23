28a1364fc5065085c0943d2c1ea65d9f496130d4
Mauri <mauri.monzon91@gmail.com>
2023-08-23 13:29:56 -0300

    Editar tareas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import TaskCard from "@/components/TaskCard";
+
 async function loadTask() {
   const res = await fetch("http://localhost:3000/api/tasks");
   const data = await res.json();
@@ -11,12 +13,7 @@ async function HomePage() {
     <section className="container mx-auto">
       <div className="grid grid-cols-3 gap-3 mt-10">
         {tasks.map((task) => (
-          <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
-            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
-            <p>{task.description}</p>
-            {/* Como es un objeto primero hay que convertirlo */}
-            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
-          </div>
+          <TaskCard task={task} key={task.id} />
         ))}
       </div>
     </section>
