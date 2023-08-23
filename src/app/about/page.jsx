09ef792036921c1de9efde091ae20cc2127a0202
Mauri <mauri.monzon91@gmail.com>
2023-08-23 19:35:36 -0300

    PageAbout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,15 @@
 function AboutPage() {
-    return <div>AboutPage</div>;
-  }
-  
-  export default AboutPage;
-  
+  return (
+    <section className="container mx-auto">
+      <h3 className="text-4xl font-bold my-5">AboutPage</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+        accusantium ex odit labore temporibus debitis explicabo, repellendus
+        mollitia maxime impedit sequi dolore quis aliquam dolores voluptatum ut.
+        Ad, quaerat ex.
+      </p>
+    </section>
+  );
+}
+
+export default AboutPage;
