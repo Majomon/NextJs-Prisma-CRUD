f9928d82f15ad0306c3b3f64508a94932ef52689
Mauri <mauri.monzon91@gmail.com>
2023-08-24 10:57:20 -0300

    Configurando para el deploy

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -26,20 +26,21 @@ function NewPage({ params }) {
         method: "PUT",
         body: JSON.stringify({ title, description }),
         headers: {
-          contentType: "applocation/json",
+          contentType: "application/json",
         },
       });
       const data = await res.json();
       console.log(data);
     } else {
-      const res = await fetch("/api/tasks", {
+      const res = await fetch(`/api/tasks`, {
         method: "POST",
         body: JSON.stringify({ title, description }),
         headers: {
-          contentType: "applocation/json",
+          contentType: "application/json",
         },
       });
       const data = await res.json();
+      console.log(data);
     }
     //Para hacer un nuevo pull de datos - Refrescar la pagina asi hace la peticion fetch
     router.refresh();
@@ -72,10 +73,7 @@ function NewPage({ params }) {
           value={description}
         ></textarea>
         <div className="flex justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Crear
           </button>
           {params.id && (
